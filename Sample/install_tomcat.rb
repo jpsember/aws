13cd299d00cfda5680b2a5f0cb5381470e7797a7
Jeff Sember <jpsember@gmail.com>
2015-05-21 11:54:38 -0700

    Issue #4: minor changes to install tomcat script

--- Sample/install_tomcat.rb
+++ Sample/install_tomcat.rb
@@ -1,10 +1,16 @@
 #!/usr/bin/env ruby
-
 require 'js_base'
 
-scall("brew install tomcat")
+puts "Using homebrew to install tomcat, and create symbolic link from ~/tomcat"
+
 USER = ENV['USER']
 HOME_DIR = File.expand_path('~')
 TOMCAT_DIR = File.join(HOME_DIR,"tomcat")
+
+if File.symlink?(TOMCAT_DIR)
+  File.unlink(TOMCAT_DIR)
+end
+
+scall("brew install tomcat")
 scall("sudo ln -s /usr/local/Cellar/tomcat/8.0.22/libexec #{TOMCAT_DIR}")
 scall("sudo chown -R #{USER} #{TOMCAT_DIR}")
