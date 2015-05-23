588a50f72f6ebb02a0202e3b0e2977cb58dcd26b
Jeff Sember <jpsember@gmail.com>
2015-05-22 18:12:49 -0700

    Issue #6: Using Ruby methods for some calls

--- Sample/build_servlet.rb
+++ Sample/build_servlet.rb
@@ -3,11 +3,14 @@ require 'js_base'
 
 HOME_DIR = File.expand_path('~')
 TOMCAT_DIR = File.join(HOME_DIR,"tomcat")
+WEBAPPS_DIR = File.join(TOMCAT_DIR,"webapps")
+
+die "No webapps directory at #{WEBAPPS_DIR}" if !File.directory?(WEBAPPS_DIR)
 
 puts "Building servlet .war file, copying to tomcat directory"
 
 scall("gradle war -q")
-scall("cp build/libs/Sample.war #{TOMCAT_DIR}/webapps")
+FileUtils.cp("build/libs/Sample.war",WEBAPPS_DIR)
 
 puts
 puts "Servlet can be found at: http://localhost:8080/Sample"
