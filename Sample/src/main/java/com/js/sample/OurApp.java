04837ff09f123cbd5cfaf63ea1c11c6ed5737b8d
Jeff Sember <jpsember@gmail.com>
2015-05-24 21:49:04 -0700

    Added AJAX components; not running yet, some sort of Tomcat problem

--- Sample/src/main/java/com/js/sample/OurApp.java
+++ Sample/src/main/java/com/js/sample/OurApp.java
@@ -3,6 +3,7 @@ package com.js.sample;
 import org.apache.wicket.protocol.http.WebApplication;
 
 public class OurApp extends WebApplication {
+
   public OurApp() {
   }
 
--- Sample/src/main/java/com/js/sample/OurWebPage.java
+++ Sample/src/main/java/com/js/sample/OurWebPage.java
@@ -2,9 +2,32 @@ package com.js.sample;
 
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.Model;
 
 public class OurWebPage extends WebPage {
+
   public OurWebPage() {
-    add(new Label("message", "Hello World!"));
-    }
+    mLabel = new Label("message");
+    add(mLabel);
+
+    Form form = new Form("login_form");
+    add(form);
+    mLoginIdField = new TextField("login_id", new Model(""));
+    form.add(mLoginIdField);
+    form.add(new Button("login_button") {
+      @Override
+      public void onSubmit() {
+        String value = (String) mLoginIdField.getModelObject();
+        mLabel.setDefaultModelObject(value);
+      }
+    });
+
+  }
+
+  private Label mLabel;
+  private TextField mLoginIdField;
+
 }
